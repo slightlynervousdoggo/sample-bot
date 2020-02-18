bed029ee501b31b1783391f4d0017fc14e0b7be5
Slightly Nervous Doggo <github@slightlynervousdoggo.com>
2020-02-17 21:13:04 -0500

    Added kick and ban commands

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # Mirror
 ### Bot, API and Dashboard for Discord Bot
 
-More information coming soon...
+### Enviromental Variables
+
+- **Development**: Change the `.env.example` file to `.env` and fill in the environmental variables
+- **Production**: Install pm2 globally `npm i pm2 -g`, change the `ecosystem.config.js.example` file to `ecosystem.config.js` and fill in the environmental variables
+- **Docker**: Use an .env file with docker-compose. Docker setup coming soon...
+
+### Installation
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+$ cd mirror
+$ npm install -d
+$ npm run server
+```
+
+For production environments...
+
+```sh
+$ npm install --production
+$ npm start
+```
--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const PREFIX = process.env.PREFIX;
+const ROLESWITHKICKPERMISSION = process.env.ROLESWITHKICKPERMISSION
+const ROLESWITHBANPERMISSION = process.env.ROLESWITHBANPERMISSION;
 
 const startBot = async () => {
   try {
@@ -26,9 +28,65 @@ const startBot = async () => {
         .split(/ +/g);
       const command = args.shift().toLowerCase();
 
-      if (command === 'profile') {
+      if (command === 'kick') {
+        if (!message.member.roles.some(r => ROLESWITHKICKPERMISSION.includes(r.id))) {
+          return message.reply("Sorry, you don't have permissions to use this command!")
+        }
+
+        const member = message.mentions.members.first() || message.guild.members.get(args[0]);
+
+        // Check if the user is in the server
+        if (!member) {
+          return message.reply("User is not in this server")
+        }
+
+        if (!member.kickable) {
+          return message.reply("I cannot kick this user! Do they have a higher role?");
+        }
+
+        let reason = args.slice(1).join(' ');
+        if (!reason) {
+          reason = "No reason provided"
+        }
+
+        try {
+          await member.kick(reason)
+          message.reply(`${member.user.tag} has been kicked by ${message.author.tag}. Reason: ${reason}`)
+        } catch (e) {
+          message.reply(`Sorry ${message.author} I couldnt kick because of : ${error}`)
+        }
+      }
+
+      if (command === 'ban') {
+        if (!message.member.roles.some(r => ROLESWITHBANPERMISSION.includes(r.id))) {
+          return message.reply("Sorry, you don't have permissions to use this command!")
+        }
+
+        const member = message.mentions.members.first() || message.guild.members.get(args[0]);
+
+        // Check if the user is in the server
+        if (!member) {
+          return message.reply("Member is not in this server")
+        }
+
+        if (!member.bannable) {
+          return message.reply("I cannot ban this user! Do they have a higher role?");
+        }
+
+        let reason = args.slice(1).join(' ');
+        if (!reason) {
+          reason = "No reason provided"
+        }
+
+        try {
+          await member.kick(reason)
+          message.reply(`${member.user.tag} has been banned by ${message.author.tag}. Reason: ${reason}`)
+        } catch (e) {
+          message.reply(`Sorry ${message.author} I couldnt ban because of : ${error}`)
+        }
 
       }
+
     });
 
     client.login(BOT_TOKEN);
